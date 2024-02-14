dc38e54c98500689fec1a7317058aa754526b9c6
JimmyJJr <159973218+JimmyJJr@users.noreply.github.com>
2024-02-14 12:58:26 +1100

    1.01 basic additions

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
-
-# Declare member variables here. Examples:
+var movement_speed = 200
+var bulletSource = preload ("res://Bullet/Bullet.tscn")
 # var a = 2
 # var b = "text"
 
@@ -9,8 +9,8 @@ extends KinematicBody2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _ready():
+set_process(true)
+set_physics_process (true)
 #func _process(delta):
 #	pass
