158e230cdef3c752eae99374bc006086cdd4f3b6
JimmyJJr <159973218+JimmyJJr@users.noreply.github.com>
2024-02-16 10:05:35 +1100

    1.02 movement and firing

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,11 +6,20 @@ var bulletSource = preload ("res://Bullet/Bullet.tscn")
 # var b = "text"
 
 
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-func _ready():
-set_process(true)
-set_physics_process (true)
-#func _process(delta):
-#	pass
+	set_process(true)
+	set_physics_process (true)
+func _process(delta):
+	if Input.is_action_just_pressed("fire"):
+		if GlobalVariables.bulletInstanceCount < 3:
+			var bulletInstance = bulletSource.instance()
+			bulletInstance.position = Vector2(position.x, position.y-20)
+			get_tree().get_root().add_child(bulletInstance)
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_left"):
+		move_and_collide(Vector2(-movement_speed * delta, 0))
+	if Input.is_action_pressed("ui_right"):
+		move_and_collide(Vector2(movement_speed * delta, 0))
