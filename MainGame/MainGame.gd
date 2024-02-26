4e61a28f8ed633c4b230eb0a2e267d8015cb4d15
JimmyJJr <159973218+JimmyJJr@users.noreply.github.com>
2024-02-26 12:53:21 +1100

    1.05 console prints womp womp upon timer reaching 0

--- MainGame/MainGame.gd
+++ MainGame/MainGame.gd
@@ -7,11 +7,10 @@ var currentTimer
 func _ready():
 	currentTimer = countdownMax
 	$HUD/Countdown.text = str(currentTimer)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
-
+	
+	while currentTimer > 0:
+		yield(get_tree().create_timer(1.0), "timeout")
+		$HUD/Countdown.text = str(currentTimer)
+		currentTimer = currentTimer - 1
+		print(currentTimer)
+	print("womp womp")
